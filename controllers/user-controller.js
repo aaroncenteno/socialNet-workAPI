9a7d17e1848c36d99751c45f08b831c11c60547e
ASC8 <asamcent@gmail.com>
2020-12-13 15:30:01 -0800

    add user controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,74 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__V'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err);
+        });
+    },
+
+    // get user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+
+    //  Create a User
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Update a pizza by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Delete a user by id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found this id! '});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+}
+
+module.exports = userController;
